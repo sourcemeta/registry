3039645970d1dd7c58a71e85e47cfca09f65b9b5
Juan Cruz Viotti <jv@jviotti.com>
2024-11-12 16:05:19 -0400

    Upgrade Hydra to the latest version (#15)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/FindHydra.cmake
+++ cmake/FindHydra.cmake
@@ -1,6 +1,7 @@
 if(NOT Hydra_FOUND)
   set(HYDRA_INSTALL OFF CACHE BOOL "disable installation")
   set(HYDRA_BUCKET OFF CACHE BOOL "disable S3 bucket support")
+  set(HYDRA_HTTPCLIENT OFF CACHE BOOL "disable HTTP client support")
   add_subdirectory("${PROJECT_SOURCE_DIR}/vendor/hydra")
   set(Hydra_FOUND ON)
 endif()
