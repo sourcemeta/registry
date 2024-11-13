1a15ecfa9199e3cf0ac582bc279f62d964d48b55
Juan Cruz Viotti <jv@jviotti.com>
2024-11-13 14:42:07 -0400

    Improve HTML `<head>` metadata
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/enterprise/enterprise_explorer.h
+++ src/enterprise/enterprise_explorer.h
@@ -8,17 +8,31 @@
 
 #include <sstream> // std::ostringstream
 
-static auto explorer_start(const std::string &title) -> std::ostringstream {
+static auto
+explorer_start(const sourcemeta::hydra::http::ServerRequest &request,
+               const std::string &server_base_url, const std::string &title,
+               const std::string &description) -> std::ostringstream {
   std::ostringstream html;
   html << "<!DOCTYPE html>";
   html << "<html lang=\"en\">";
   html << "<head>";
-  html << "<title>";
-  html << title;
-  html << "</title>";
+
+  // Meta headers
+  html << "<meta charset=\"utf-8\">";
+  html << "<meta name=\"referrer\" content=\"no-referrer\">";
+  html << "<meta name=\"viewport\" content=\"width=device-width, "
+          "initial-scale=1.0\">";
+  html << "<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\">";
+
+  // Site metadata
+  html << "<title>" << title << "</title>";
+  html << "<meta name=\"description\" content=\"" << description << "\">";
+  html << "<link rel=\"canonical\" href=\"" << server_base_url << request.path()
+       << "\">";
   html << "<link rel=\"stylesheet\" href=\"/style.min.css\">";
+
   html << "</head>";
-  html << "<body>";
+  html << "<body class=\"h-100\">";
   return html;
 }
 
@@ -37,17 +51,25 @@ static auto explorer_end(std::ostringstream &html,
 
 namespace sourcemeta::registry::enterprise {
 
-auto explore_index(sourcemeta::hydra::http::ServerResponse &response) -> void {
-  std::ostringstream html{explorer_start("Sourcemeta Schemas")};
+auto explore_index(const std::string &server_base_url,
+                   const sourcemeta::hydra::http::ServerRequest &request,
+                   sourcemeta::hydra::http::ServerResponse &response) -> void {
+  std::ostringstream html{
+      explorer_start(request, server_base_url, "Sourcemeta Schemas",
+                     "The Sourcemeta JSON Schema registry")};
   html << "<div class=\"container\">";
   html << "Sourcemeta Schemas";
   html << "</div>";
   explorer_end(html, response, sourcemeta::hydra::http::Status::OK);
 }
 
-auto explore_not_found(sourcemeta::hydra::http::ServerResponse &response)
+auto explore_not_found(const std::string &server_base_url,
+                       const sourcemeta::hydra::http::ServerRequest &request,
+                       sourcemeta::hydra::http::ServerResponse &response)
     -> void {
-  std::ostringstream html{explorer_start("Not Found")};
+  std::ostringstream html{
+      explorer_start(request, server_base_url, "Not Found",
+                     "What you are looking for is not here")};
   html << "<div class=\"container\">";
   html << "Not Found";
   html << "</div>";
