15461ef0ee8f047d79592ce1078b2f9ee520c78a
Juan Cruz Viotti <jv@jviotti.com>
2025-01-06 15:35:54 -0400

    Extract HTML partials into a separate file (#79)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/enterprise/enterprise_explorer.h
+++ src/enterprise/enterprise_explorer.h
@@ -4,77 +4,14 @@
 #include <sourcemeta/hydra/http.h>
 #include <sourcemeta/hydra/httpserver.h>
 
-#include "enterprise_explorer.h"
+#include "enterprise_html.h"
 
 #include <cassert> // assert
 #include <sstream> // std::ostringstream
 
-static auto
-explorer_start(const sourcemeta::hydra::http::ServerRequest &request,
-               const std::string &server_base_url, const std::string &site_name,
-               const std::string &title, const std::string &description)
-    -> std::ostringstream {
-  std::ostringstream html;
-  html << "<!DOCTYPE html>";
-  html << "<html lang=\"en\">";
-  html << "<head>";
-
-  // Meta headers
-  html << "<meta charset=\"utf-8\">";
-  html << "<meta name=\"referrer\" content=\"no-referrer\">";
-  html << "<meta name=\"viewport\" content=\"width=device-width, "
-          "initial-scale=1.0\">";
-  html << "<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\">";
-
-  // Site metadata
-  html << "<title>" << title << "</title>";
-  html << "<meta name=\"description\" content=\"" << description << "\">";
-  html << "<link rel=\"canonical\" href=\"" << server_base_url << request.path()
-       << "\">";
-  html << "<link rel=\"stylesheet\" href=\"/style.min.css\">";
-
-  // Application icons
-  // TODO: Allow changing these by supporing an object in the
-  // configuration manifest to select static files to override
-  html << "<link rel=\"icon\" href=\"/favicon.ico\" sizes=\"any\">";
-  html << "<link rel=\"icon\" href=\"/icon.svg\" type=\"image/svg+xml\">";
-  html << "<link rel=\"shortcut icon\" type=\"image/png\" "
-          "href=\"/apple-touch-icon.png\">";
-  html << "<link rel=\"apple-touch-icon\" sizes=\"180x180\" "
-          "href=\"/apple-touch-icon.png\">";
-  html << "<link rel=\"manifest\" href=\"/manifest.webmanifest\">";
-
-  if (configuration().defines("analytics")) {
-    if (configuration().at("analytics").defines("plausible")) {
-      html << "<script defer data-domain=\"";
-      html << configuration().at("analytics").at("plausible").to_string();
-      html << "\" src=\"https://plausible.io/js/script.js\"></script>";
-    }
-  }
-
-  html << "</head>";
-  html << "<body class=\"h-100\">";
-
-  html << "<nav class=\"navbar navbar-expand border-bottom bg-body\">";
-  html << "<div class=\"container-fluid px-4 py-2\">";
-  html << "<a class=\"navbar-brand\" href=\"" << server_base_url << "\">";
-  html << "<img src=\"/icon.svg\" alt=\"" << site_name
-       << "\" height=\"30\" width=\"30\" class=\"me-2\">";
-  html << "<span class=\"align-middle fw-bold\">" << site_name << "</span>";
-  html << "<span class=\"align-middle fw-lighter\"> Schemas</span>";
-  html << "</a>";
-  html << "</div>";
-  html << "</nav>";
-
-  return html;
-}
-
 static auto explorer_end(std::ostringstream &html,
                          sourcemeta::hydra::http::ServerResponse &response,
                          const sourcemeta::hydra::http::Status code) -> void {
-  html << "</body>";
-  html << "</html>";
-
   // TODO: On non-debug builds, add at least some sane level
   // of HTTP caching, maybe similar to how GitHub Pages does it?
   response.status(code);
@@ -82,190 +19,45 @@ static auto explorer_end(std::ostringstream &html,
   response.end(html.str());
 }
 
-static auto file_manager(std::ostringstream &html,
-                         const std::filesystem::path &directory) -> void {
-  const auto meta_path{directory / "index.json"};
-  assert(std::filesystem::exists(meta_path));
-  const auto meta{sourcemeta::jsontoolkit::from_file(meta_path)};
-
-  assert(meta.defines("breadcrumb"));
-  assert(meta.at("breadcrumb").is_array());
-  if (!meta.at("breadcrumb").empty()) {
-    html << "<nav class=\"container-fluid px-4 py-2 bg-light bg-gradient "
-            "border-bottom font-monospace\" aria-label=\"breadcrumb\">";
-    html << "<ol class=\"breadcrumb mb-0\">";
-    html << "<li class=\"breadcrumb-item\">";
-    html << "<a href=\"/\">";
-    html << "<i class=\"bi bi-arrow-left\"></i>";
-    html << "</a>";
-    html << "</li>";
-
-    const auto &breadcrumb{meta.at("breadcrumb").as_array()};
-    for (auto iterator = breadcrumb.cbegin(); iterator != breadcrumb.cend();
-         iterator++) {
-      assert(iterator->is_object());
-      assert(iterator->defines("name"));
-      assert(iterator->at("name").is_string());
-      assert(iterator->defines("url"));
-      assert(iterator->at("url").is_string());
-      if (std::next(iterator) == breadcrumb.cend()) {
-        html << "<li class=\"breadcrumb-item active\" aria-current=\"page\">";
-        html << iterator->at("name").to_string();
-        html << "</li>";
-      } else {
-        html << "<li class=\"breadcrumb-item\">";
-        html << "<a href=\"" << iterator->at("url").to_string() << "\">";
-        html << iterator->at("name").to_string();
-        html << "</a>";
-        html << "</li>";
-      }
-    }
-
-    html << "</ol>";
-    html << "</nav>";
-  }
-
-  html << "<div class=\"container-fluid p-4\">";
-  html << "<table class=\"table table-bordered border-light-subtle "
-          "table-light\">";
-
-  html << "<thead>";
-  html << "<tr>";
-  html << "<th scope=\"col\" style=\"width: 50px\"></th>";
-  html << "<th scope=\"col\">Name</th>";
-  html << "<th scope=\"col\">Title</th>";
-  html << "<th scope=\"col\">Description</th>";
-  html << "</tr>";
-  html << "</thead>";
-  html << "<tbody>";
-
-  assert(meta.is_object());
-  assert(meta.defines("entries"));
-  assert(meta.at("entries").is_array());
-
-  for (const auto &entry : meta.at("entries").as_array()) {
-    assert(entry.is_object());
-    html << "<tr>";
-
-    assert(entry.defines("type"));
-    assert(entry.at("type").is_string());
-    html << "<td class=\"text-nowrap\">";
-    if (entry.at("type").to_string() == "directory") {
-      if (entry.defines("github")) {
-        html << "<img src=\"https://github.com/";
-        html << entry.at("github").to_string();
-        html << ".png?size=80\" width=\"40\" height=\"40\">";
-      } else {
-        html << "<i class=\"bi bi-folder-fill\"></i>";
-      }
-    } else {
-      const auto &base_dialect{entry.at("baseDialect").to_string()};
-      html << "<a "
-              "href=\"https://www.learnjsonschema.com/";
-      html << base_dialect;
-      html << "\">";
-      html << "<span class=\"align-middle badge ";
-
-      // Highlight the latest version in a different manner
-      if (base_dialect == "2020-12") {
-        html << "text-bg-primary";
-      } else {
-        html << "text-bg-danger";
-      }
-
-      html << "\">";
-
-      for (auto iterator = base_dialect.cbegin();
-           iterator != base_dialect.cend(); ++iterator) {
-        if (iterator == base_dialect.cbegin()) {
-          html << static_cast<char>(std::toupper(*iterator));
-        } else {
-          html << *iterator;
-        }
-      }
-
-      html << "</span>";
-      html << "</a>";
-    }
-    html << "</td>";
-
-    assert(entry.defines("name"));
-    assert(entry.at("name").is_string());
-    html << "<td class=\"font-monospace text-nowrap\">";
-    assert(entry.defines("url"));
-    assert(entry.at("url").is_string());
-    html << "<a href=\"" << entry.at("url").to_string() << "\">";
-    html << entry.at("name").to_string();
-    html << "</a>";
-    html << "</td>";
-
-    html << "<td>";
-    html << "<small>";
-    if (entry.defines("title")) {
-      html << entry.at("title").to_string();
-    } else {
-      html << "-";
-    }
-    html << "</small>";
-    html << "</td>";
-
-    html << "<td>";
-    html << "<small>";
-    if (entry.defines("description")) {
-      html << entry.at("description").to_string();
-    } else {
-      html << "-";
-    }
-    html << "</small>";
-    html << "</td>";
-
-    html << "</tr>";
-  }
-
-  html << "</tbody>";
-
-  html << "</table>";
-  html << "</div>";
-}
-
 namespace sourcemeta::registry::enterprise {
 
-auto explore_index(const std::string &site_name, const std::string &title,
-                   const std::string &description,
-                   const std::string &server_base_url,
+auto explore_index(const std::string &title, const std::string &description,
                    const std::filesystem::path &schema_base_directory,
                    const sourcemeta::hydra::http::ServerRequest &request,
                    sourcemeta::hydra::http::ServerResponse &response) -> void {
-  std::ostringstream html{
-      explorer_start(request, server_base_url, site_name, title, description)};
-  file_manager(html, sourcemeta::registry::path_join(schema_base_directory,
-                                                     request.path()));
+  std::ostringstream html;
+  sourcemeta::registry::enterprise::html_start(html, configuration(), title,
+                                               description, request.path());
+  sourcemeta::registry::enterprise::html_file_manager(
+      html,
+      sourcemeta::registry::path_join(schema_base_directory, request.path()));
+  sourcemeta::registry::enterprise::html_end(html);
   explorer_end(html, response, sourcemeta::hydra::http::Status::OK);
 }
 
-auto explore_directory(const std::string &site_name,
-                       const std::filesystem::path &directory,
-                       const std::string &server_base_url,
+auto explore_directory(const std::filesystem::path &directory,
                        const sourcemeta::hydra::http::ServerRequest &request,
                        sourcemeta::hydra::http::ServerResponse &response)
     -> void {
-  std::ostringstream html{explorer_start(request, server_base_url, site_name,
-                                         request.path(), request.path())};
-  file_manager(html, directory);
+  std::ostringstream html;
+  sourcemeta::registry::enterprise::html_start(
+      html, configuration(), request.path(), request.path(), request.path());
+  sourcemeta::registry::enterprise::html_file_manager(html, directory);
+  sourcemeta::registry::enterprise::html_end(html);
   explorer_end(html, response, sourcemeta::hydra::http::Status::OK);
 }
 
-auto explore_not_found(const std::string &site_name,
-                       const std::string &server_base_url,
-                       const sourcemeta::hydra::http::ServerRequest &request,
+auto explore_not_found(const sourcemeta::hydra::http::ServerRequest &request,
                        sourcemeta::hydra::http::ServerResponse &response)
     -> void {
-  std::ostringstream html{
-      explorer_start(request, server_base_url, site_name, "Not Found",
-                     "What you are looking for is not here")};
+  std::ostringstream html;
+  sourcemeta::registry::enterprise::html_start(
+      html, configuration(), "Not Found",
+      "What you are looking for is not here", request.path());
   html << "<div class=\"container-fluid p-4\">";
   html << "Not Found";
   html << "</div>";
+  sourcemeta::registry::enterprise::html_end(html);
   explorer_end(html, response, sourcemeta::hydra::http::Status::NOT_FOUND);
 }
 
---
+++ src/enterprise/enterprise_html.h
@@ -0,0 +1,228 @@
+#ifndef SOURCEMETA_REGISTRY_ENTERPRISE_HTML_H_
+#define SOURCEMETA_REGISTRY_ENTERPRISE_HTML_H_
+
+#include <sourcemeta/jsontoolkit/json.h>
+
+#include <filesystem> // std::filesystem
+#include <optional>   // std::optional
+#include <sstream>    // std::ostringstream
+#include <string>     // std::string
+
+namespace sourcemeta::registry::enterprise {
+
+auto html_start(std::ostringstream &html,
+                const sourcemeta::jsontoolkit::JSON &configuration,
+                const std::string &title, const std::string &description,
+                const std::optional<std::string> &path) -> void {
+  const auto &base_url{configuration.at("url").to_string()};
+
+  html << "<!DOCTYPE html>";
+  html << "<html lang=\"en\">";
+  html << "<head>";
+
+  // Meta headers
+  html << "<meta charset=\"utf-8\">";
+  html << "<meta name=\"referrer\" content=\"no-referrer\">";
+  html << "<meta name=\"viewport\" content=\"width=device-width, "
+          "initial-scale=1.0\">";
+  html << "<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\">";
+
+  // Site metadata
+  html << "<title>" << title << "</title>";
+  html << "<meta name=\"description\" content=\"" << description << "\">";
+  if (path.has_value()) {
+    html << "<link rel=\"canonical\" href=\"" << base_url << path.value()
+         << "\">";
+  }
+  html << "<link rel=\"stylesheet\" href=\"/style.min.css\">";
+
+  // Application icons
+  // TODO: Allow changing these by supporing an object in the
+  // configuration manifest to select static files to override
+  html << "<link rel=\"icon\" href=\"/favicon.ico\" sizes=\"any\">";
+  html << "<link rel=\"icon\" href=\"/icon.svg\" type=\"image/svg+xml\">";
+  html << "<link rel=\"shortcut icon\" type=\"image/png\" "
+          "href=\"/apple-touch-icon.png\">";
+  html << "<link rel=\"apple-touch-icon\" sizes=\"180x180\" "
+          "href=\"/apple-touch-icon.png\">";
+  html << "<link rel=\"manifest\" href=\"/manifest.webmanifest\">";
+
+  if (configuration.defines("analytics")) {
+    if (configuration.at("analytics").defines("plausible")) {
+      html << "<script defer data-domain=\"";
+      html << configuration.at("analytics").at("plausible").to_string();
+      html << "\" src=\"https://plausible.io/js/script.js\"></script>";
+    }
+  }
+
+  html << "</head>";
+  html << "<body class=\"h-100\">";
+
+  const auto &site_name{configuration.at("title").to_string()};
+
+  html << "<nav class=\"navbar navbar-expand border-bottom bg-body\">";
+  html << "<div class=\"container-fluid px-4 py-2\">";
+  html << "<a class=\"navbar-brand\" href=\"" << base_url << "\">";
+  html << "<img src=\"/icon.svg\" alt=\"" << site_name
+       << "\" height=\"30\" width=\"30\" class=\"me-2\">";
+  html << "<span class=\"align-middle fw-bold\">" << site_name << "</span>";
+  html << "<span class=\"align-middle fw-lighter\"> Schemas</span>";
+  html << "</a>";
+  html << "</div>";
+  html << "</nav>";
+}
+
+auto html_end(std::ostringstream &html) -> void {
+  html << "</body>";
+  html << "</html>";
+}
+
+auto html_file_manager(std::ostringstream &html,
+                       const std::filesystem::path &directory) -> void {
+  const auto meta_path{directory / "index.json"};
+  assert(std::filesystem::exists(meta_path));
+  const auto meta{sourcemeta::jsontoolkit::from_file(meta_path)};
+
+  assert(meta.defines("breadcrumb"));
+  assert(meta.at("breadcrumb").is_array());
+  if (!meta.at("breadcrumb").empty()) {
+    html << "<nav class=\"container-fluid px-4 py-2 bg-light bg-gradient "
+            "border-bottom font-monospace\" aria-label=\"breadcrumb\">";
+    html << "<ol class=\"breadcrumb mb-0\">";
+    html << "<li class=\"breadcrumb-item\">";
+    html << "<a href=\"/\">";
+    html << "<i class=\"bi bi-arrow-left\"></i>";
+    html << "</a>";
+    html << "</li>";
+
+    const auto &breadcrumb{meta.at("breadcrumb").as_array()};
+    for (auto iterator = breadcrumb.cbegin(); iterator != breadcrumb.cend();
+         iterator++) {
+      assert(iterator->is_object());
+      assert(iterator->defines("name"));
+      assert(iterator->at("name").is_string());
+      assert(iterator->defines("url"));
+      assert(iterator->at("url").is_string());
+      if (std::next(iterator) == breadcrumb.cend()) {
+        html << "<li class=\"breadcrumb-item active\" aria-current=\"page\">";
+        html << iterator->at("name").to_string();
+        html << "</li>";
+      } else {
+        html << "<li class=\"breadcrumb-item\">";
+        html << "<a href=\"" << iterator->at("url").to_string() << "\">";
+        html << iterator->at("name").to_string();
+        html << "</a>";
+        html << "</li>";
+      }
+    }
+
+    html << "</ol>";
+    html << "</nav>";
+  }
+
+  html << "<div class=\"container-fluid p-4\">";
+  html << "<table class=\"table table-bordered border-light-subtle "
+          "table-light\">";
+
+  html << "<thead>";
+  html << "<tr>";
+  html << "<th scope=\"col\" style=\"width: 50px\"></th>";
+  html << "<th scope=\"col\">Name</th>";
+  html << "<th scope=\"col\">Title</th>";
+  html << "<th scope=\"col\">Description</th>";
+  html << "</tr>";
+  html << "</thead>";
+  html << "<tbody>";
+
+  assert(meta.is_object());
+  assert(meta.defines("entries"));
+  assert(meta.at("entries").is_array());
+
+  for (const auto &entry : meta.at("entries").as_array()) {
+    assert(entry.is_object());
+    html << "<tr>";
+
+    assert(entry.defines("type"));
+    assert(entry.at("type").is_string());
+    html << "<td class=\"text-nowrap\">";
+    if (entry.at("type").to_string() == "directory") {
+      if (entry.defines("github")) {
+        html << "<img src=\"https://github.com/";
+        html << entry.at("github").to_string();
+        html << ".png?size=80\" width=\"40\" height=\"40\">";
+      } else {
+        html << "<i class=\"bi bi-folder-fill\"></i>";
+      }
+    } else {
+      const auto &base_dialect{entry.at("baseDialect").to_string()};
+      html << "<a "
+              "href=\"https://www.learnjsonschema.com/";
+      html << base_dialect;
+      html << "\">";
+      html << "<span class=\"align-middle badge ";
+
+      // Highlight the latest version in a different manner
+      if (base_dialect == "2020-12") {
+        html << "text-bg-primary";
+      } else {
+        html << "text-bg-danger";
+      }
+
+      html << "\">";
+
+      for (auto iterator = base_dialect.cbegin();
+           iterator != base_dialect.cend(); ++iterator) {
+        if (iterator == base_dialect.cbegin()) {
+          html << static_cast<char>(std::toupper(*iterator));
+        } else {
+          html << *iterator;
+        }
+      }
+
+      html << "</span>";
+      html << "</a>";
+    }
+    html << "</td>";
+
+    assert(entry.defines("name"));
+    assert(entry.at("name").is_string());
+    html << "<td class=\"font-monospace text-nowrap\">";
+    assert(entry.defines("url"));
+    assert(entry.at("url").is_string());
+    html << "<a href=\"" << entry.at("url").to_string() << "\">";
+    html << entry.at("name").to_string();
+    html << "</a>";
+    html << "</td>";
+
+    html << "<td>";
+    html << "<small>";
+    if (entry.defines("title")) {
+      html << entry.at("title").to_string();
+    } else {
+      html << "-";
+    }
+    html << "</small>";
+    html << "</td>";
+
+    html << "<td>";
+    html << "<small>";
+    if (entry.defines("description")) {
+      html << entry.at("description").to_string();
+    } else {
+      html << "-";
+    }
+    html << "</small>";
+    html << "</td>";
+
+    html << "</tr>";
+  }
+
+  html << "</tbody>";
+
+  html << "</table>";
+  html << "</div>";
+}
+
+} // namespace sourcemeta::registry::enterprise
+
+#endif
