80ad02da342c680de5aa866f063ba32a9d8116b7
Juan Cruz Viotti <jv@jviotti.com>
2025-01-06 16:24:22 -0400

    Inline `error.h` in `src/server` (#82)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/enterprise/enterprise_server.h
+++ src/enterprise/enterprise_server.h
@@ -58,8 +58,6 @@ auto attach(sourcemeta::hydra::http::Server &server) -> void {
   server.route(sourcemeta::hydra::http::Method::GET, "/", on_index);
   server.route(sourcemeta::hydra::http::Method::GET, "/*", on_request);
   server.route(sourcemeta::hydra::http::Method::HEAD, "/*", on_request);
-  server.otherwise(::on_otherwise);
-  server.error(sourcemeta::registry::on_error);
 }
 
 } // namespace sourcemeta::registry::enterprise
