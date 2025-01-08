93751aad8d29a56162950d6d33c360db2ee1b49e
Juan Cruz Viotti <jv@jviotti.com>
2025-01-08 13:13:35 -0400

    Implement utilities to safely create HTML content
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/html/include/sourcemeta/registry/html_partials.h
@@ -0,0 +1,40 @@
+#ifndef SOURCEMETA_REGISTRY_HTML_PARTIALS_H_
+#define SOURCEMETA_REGISTRY_HTML_PARTIALS_H_
+
+#include <string_view> // std::string_view
+
+namespace sourcemeta::registry::html::partials {
+template <typename T> auto css(T &output, const std::string_view url) -> T & {
+  output.open("link", {{"rel", "stylesheet"}, {"href", url}});
+  return output;
+}
+
+template <typename T>
+auto image(T &output, const std::string_view url, const std::uint64_t height,
+           const std::uint64_t width, const std::string_view alt,
+           const std::optional<std::string_view> classes) -> T & {
+  if (classes.has_value()) {
+    output.open("img", {{"src", url},
+                        {"alt", alt},
+                        {"height", std::to_string(height)},
+                        {"width", std::to_string(width)},
+                        {"class", classes.value()}});
+  } else {
+    output.open("img", {{"src", url},
+                        {"alt", alt},
+                        {"height", std::to_string(height)},
+                        {"width", std::to_string(width)}});
+  }
+
+  return output;
+}
+
+template <typename T>
+auto image(T &output, const std::string_view url, const std::uint64_t size,
+           const std::string_view alt,
+           const std::optional<std::string_view> classes) -> T & {
+  return image(output, url, size, size, alt, classes);
+}
+} // namespace sourcemeta::registry::html::partials
+
+#endif
