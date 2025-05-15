463647e59d4075241257a0cf452c1a1f5f8ede95
Juan Cruz Viotti <jv@jviotti.com>
2025-05-14 20:19:46 -0400

    Rename `Configuration` to `RegistryConfiguration` (#156)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/index/configuration.cc
+++ src/index/configuration.cc
@@ -2,7 +2,7 @@
 
 #include <utility> // std::move
 
-Configuration::Configuration(std::filesystem::path path)
+RegistryConfiguration::RegistryConfiguration(std::filesystem::path path)
     : path_{std::move(path)}, base_{this->path_.parent_path()},
       data_{sourcemeta::core::read_json(this->path_)} {
   // TODO: Perform this defaulting using Blaze and "default" annotations
@@ -14,31 +14,34 @@ Configuration::Configuration(std::filesystem::path path)
       this->schema_.at("properties").at("description").at("default"));
 }
 
-auto Configuration::path() const noexcept -> const std::filesystem::path & {
+auto RegistryConfiguration::path() const noexcept
+    -> const std::filesystem::path & {
   return this->path_;
 }
 
-auto Configuration::path(const std::filesystem::path &other) const
+auto RegistryConfiguration::path(const std::filesystem::path &other) const
     -> std::filesystem::path {
   return std::filesystem::canonical(this->base_ / other);
 }
 
-auto Configuration::get() const noexcept -> const sourcemeta::core::JSON & {
+auto RegistryConfiguration::get() const noexcept
+    -> const sourcemeta::core::JSON & {
   return this->data_;
 }
 
-auto Configuration::schema() const noexcept -> const sourcemeta::core::JSON & {
+auto RegistryConfiguration::schema() const noexcept
+    -> const sourcemeta::core::JSON & {
   return this->schema_;
 }
 
-auto Configuration::url() const -> const sourcemeta::core::URI & {
+auto RegistryConfiguration::url() const -> const sourcemeta::core::URI & {
   static const sourcemeta::core::URI server_uri{
       sourcemeta::core::URI{this->data_.at("url").to_string()}.canonicalize()};
   return server_uri;
 }
 
 // For faster processing by the server component
-auto Configuration::summary() const -> sourcemeta::core::JSON {
+auto RegistryConfiguration::summary() const -> sourcemeta::core::JSON {
   auto copy = this->data_;
   copy.erase_keys({"schemas", "pages"});
   return copy;
