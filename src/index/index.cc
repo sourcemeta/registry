12ad0a36162b509abce5517932caee2ed1e04aa7
Juan Cruz Viotti <jv@jviotti.com>
2025-05-25 16:29:05 -0300

    Extract TOC generation code into a separate file
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/index/index.cc
+++ src/index/index.cc
@@ -1,182 +1,22 @@
 #include <sourcemeta/core/json.h>
 #include <sourcemeta/core/jsonschema.h>
-#include <sourcemeta/core/uri.h>
-#include <sourcemeta/core/yaml.h>
 
 #include <sourcemeta/registry/generator.h>
 
 #include "configure.h"
 #include "explorer.h"
+#include "toc.h"
 
-#include <algorithm>   // std::sort
 #include <cassert>     // assert
 #include <cstdlib>     // EXIT_FAILURE, EXIT_SUCCESS
 #include <exception>   // std::exception
 #include <filesystem>  // std::filesystem
 #include <iostream>    // std::cerr, std::cout
-#include <regex>       // std::regex, std::smatch, std::regex_search
 #include <span>        // std::span
-#include <sstream>     // std::ostringstream
 #include <string>      // std::string
 #include <string_view> // std::string_view
-#include <tuple>       // std::tuple, std::make_tuple
 #include <utility>     // std::move
 #include <vector>      // std::vector
-
-// TODO: We need a SemVer module in Core
-static auto try_parse_version(const sourcemeta::core::JSON::String &name)
-    -> std::optional<std::tuple<unsigned, unsigned, unsigned>> {
-  std::regex version_regex(R"(v?(\d+)\.(\d+)\.(\d+))");
-  std::smatch match;
-
-  if (std::regex_search(name, match, version_regex)) {
-    return std::make_tuple(std::stoul(match[1]), std::stoul(match[2]),
-                           std::stoul(match[3]));
-  }
-
-  return std::nullopt;
-}
-
-// TODO: Parse the URI instead and find the id based on the path component?
-static auto base_dialect_id(const std::string &base_dialect) -> std::string {
-  if (base_dialect == "https://json-schema.org/draft/2020-12/schema" ||
-      base_dialect == "https://json-schema.org/draft/2020-12/hyper-schema") {
-    return "2020-12";
-  }
-
-  if (base_dialect == "https://json-schema.org/draft/2019-09/schema" ||
-      base_dialect == "https://json-schema.org/draft/2019-09/hyper-schema") {
-    return "2019-09";
-  }
-
-  if (base_dialect == "http://json-schema.org/draft-07/schema#" ||
-      base_dialect == "http://json-schema.org/draft-07/hyper-schema#") {
-    return "draft7";
-  }
-
-  if (base_dialect == "http://json-schema.org/draft-06/schema#" ||
-      base_dialect == "http://json-schema.org/draft-06/hyper-schema#") {
-    return "draft6";
-  }
-
-  if (base_dialect == "http://json-schema.org/draft-04/schema#" ||
-      base_dialect == "http://json-schema.org/draft-04/hyper-schema#") {
-    return "draft4";
-  }
-
-  if (base_dialect == "http://json-schema.org/draft-03/schema#" ||
-      base_dialect == "http://json-schema.org/draft-03/hyper-schema#") {
-    return "draft3";
-  }
-
-  if (base_dialect == "http://json-schema.org/draft-02/hyper-schema#") {
-    return "draft2";
-  }
-
-  if (base_dialect == "http://json-schema.org/draft-01/hyper-schema#") {
-    return "draft1";
-  }
-
-  if (base_dialect == "http://json-schema.org/draft-00/hyper-schema#") {
-    return "draft0";
-  }
-
-  // We should never get here
-  assert(false);
-  return "Unknown";
-}
-
-auto generate_toc(const sourcemeta::registry::Configuration &configuration,
-                  sourcemeta::registry::Output &output,
-                  const sourcemeta::core::SchemaResolver &resolver,
-                  const std::filesystem::path &base,
-                  const std::filesystem::path &directory) -> void {
-  const auto server_url_string{configuration.url().recompose()};
-  assert(directory.string().starts_with(base.string()));
-  auto entries{sourcemeta::core::JSON::make_array()};
-
-  for (const auto &entry : std::filesystem::directory_iterator{directory}) {
-    auto entry_json{sourcemeta::core::JSON::make_object()};
-    entry_json.assign("name", sourcemeta::core::JSON{entry.path().filename()});
-    const auto entry_relative_path{
-        entry.path().string().substr(base.string().size() + 1)};
-    assert(!entry_relative_path.starts_with('/'));
-    if (entry.is_directory()) {
-      entry_json.merge(configuration.page(entry_relative_path));
-      entry_json.assign("type", sourcemeta::core::JSON{"directory"});
-      entry_json.assign(
-          "url", sourcemeta::core::JSON{
-                     entry.path().string().substr(base.string().size())});
-      entries.push_back(std::move(entry_json));
-    } else if (entry.path().extension() == ".json" &&
-               !entry.path().stem().string().starts_with(".")) {
-      const auto schema{sourcemeta::core::read_json(entry.path())};
-      entry_json.assign("type", sourcemeta::core::JSON{"schema"});
-      if (schema.is_object()) {
-        if (schema.defines("title")) {
-          entry_json.assign("title",
-                            sourcemeta::core::JSON{schema.at("title").trim()});
-        }
-
-        if (schema.defines("description")) {
-          entry_json.assign(
-              "description",
-              sourcemeta::core::JSON{schema.at("description").trim()});
-        }
-      }
-
-      const auto base_dialect{sourcemeta::core::base_dialect(schema, resolver)};
-      assert(base_dialect.has_value());
-      entry_json.assign("baseDialect", sourcemeta::core::JSON{base_dialect_id(
-                                           base_dialect.value())});
-      entry_json.assign("url",
-                        sourcemeta::core::JSON{"/" + entry_relative_path});
-      entries.push_back(std::move(entry_json));
-    }
-  }
-
-  std::sort(
-      entries.as_array().begin(), entries.as_array().end(),
-      [](const auto &left, const auto &right) {
-        if (left.at("type") == right.at("type")) {
-          const auto &left_name{left.at("name")};
-          const auto &right_name{right.at("name")};
-
-          // If the schema/directories represent SemVer versions, attempt to
-          // parse them as such and provide better sorting
-          const auto left_version{try_parse_version(left_name.to_string())};
-          const auto right_version{try_parse_version(right_name.to_string())};
-          if (left_version.has_value() && right_version.has_value()) {
-            return left_version.value() > right_version.value();
-          }
-
-          return left_name > right_name;
-        }
-
-        return left.at("type") < right.at("type");
-      });
-
-  auto meta{sourcemeta::core::JSON::make_object()};
-  meta.merge(
-      configuration.page(std::filesystem::relative(directory, base).string()));
-  meta.assign("entries", std::move(entries));
-
-  // Precompute the breadcrumb
-  const std::filesystem::path relative_path{directory.string().substr(
-      std::min(base.string().size() + 1, directory.string().size()))};
-  meta.assign("breadcrumb", sourcemeta::core::JSON::make_array());
-  std::filesystem::path current_path{"/"};
-  for (const auto &part : relative_path) {
-    current_path = current_path / part;
-    auto breadcrumb_entry{sourcemeta::core::JSON::make_object()};
-    breadcrumb_entry.assign("name", sourcemeta::core::JSON{part});
-    breadcrumb_entry.assign("url", sourcemeta::core::JSON{current_path});
-    meta.at("breadcrumb").push_back(std::move(breadcrumb_entry));
-  }
-
-  output.write_generated_json(
-      std::filesystem::relative(directory, base) / "index.json", meta);
-}
 
 static auto index_main(const std::string_view &program,
                        const std::span<const std::string> &arguments) -> int {
@@ -345,16 +185,21 @@ static auto index_main(const std::string_view &program,
   // (6) Generate schema navigation indexes
   // --------------------------------------------
 
-  std::cerr << "Indexing: " << output_path.string() << "\n";
   const auto base{
       output.absolute_path(sourcemeta::registry::Output::Category::Schemas)};
-  generate_toc(configuration, output, resolver, base, base);
+  std::cerr << "Indexing: " << base.string() << "\n";
+  output.write_generated_json(
+      "index.json",
+      sourcemeta::registry::toc(configuration, resolver, base, base));
   for (const auto &entry :
        std::filesystem::recursive_directory_iterator{base}) {
     if (entry.is_directory()) {
       std::cerr << "Indexing: " << entry.path().string() << "\n";
-      generate_toc(configuration, output, resolver, base,
-                   std::filesystem::canonical(entry.path()));
+      auto toc{sourcemeta::registry::toc(configuration, resolver, base,
+                                         entry.path())};
+      output.write_generated_json(
+          std::filesystem::relative(entry.path(), base) / "index.json",
+          std::move(toc));
     }
   }
 
@@ -363,7 +208,7 @@ static auto index_main(const std::string_view &program,
   // --------------------------------------------
 
   // TODO: Make the explorer generator use the Output class to writing files
-  registry_explorer_generate(
+  sourcemeta::registry::explorer(
       configuration.get(),
       output.absolute_path(sourcemeta::registry::Output::Category::Generated),
       [](const auto &path) {
