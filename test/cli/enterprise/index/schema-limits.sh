1b587db3c476e8eb29c831aae6644e9c7205ea6c
Juan Cruz Viotti <jv@jviotti.com>
2025-06-10 15:34:46 -0400

    Test schema limits for each edition (#194)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/cli/enterprise/index/schema-limits.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << EOF > "$TMP/configuration.json"
+{
+  "url": "http://localhost:8000",
+  "port": 8000,
+  "schemas": {
+    "example": {
+      "base": "https://example.com",
+      "path": "./schemas"
+    }
+  }
+}
+EOF
+
+mkdir "$TMP/schemas"
+for count in $(seq -w 1 1000)
+do
+  cat << EOF > "$TMP/schemas/$count.json"
+  {
+    "\$schema": "http://json-schema.org/draft-07/schema#",
+    "\$id": "https://example.com/$count.json"
+  }
+EOF
+done
+
+cat << 'EOF' > "$TMP/schemas/exceeding.json"
+{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "$id": "https://example.com/exceeding.json"
+}
+EOF
+
+"$1" "$TMP/configuration.json" "$TMP/dist"
