77a711f0cd36d0cf59b2eaf5581612cbba7c68fe
Juan Cruz Viotti <jv@jviotti.com>
2025-07-02 14:41:52 -0400

    Decouple the server component from the URI class (#219)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/server/server.cc
+++ src/server/server.cc
@@ -5,8 +5,6 @@
 #include <sourcemeta/registry/license.h>
 
 #include <sourcemeta/core/json.h>
-#include <sourcemeta/core/jsonschema.h>
-#include <sourcemeta/core/uri.h>
 
 #include "configure.h"
 
@@ -25,57 +23,6 @@
 #include <utility>     // std::move
 
 static inline std::unique_ptr<const std::filesystem::path> __global_data;
-static auto configuration() -> const sourcemeta::core::JSON & {
-  static auto document{
-      sourcemeta::core::read_json(*(__global_data) / "configuration.json")};
-  return document;
-}
-
-static auto path_join(const std::filesystem::path &base,
-                      const std::filesystem::path &path)
-    -> std::filesystem::path {
-  if (path.is_absolute()) {
-    return (base / path.string().substr(1)).lexically_normal();
-  }
-
-  return (base / path).lexically_normal();
-}
-
-static auto schema_directory(const bool bundle, const bool unidentify) noexcept
-    -> std::filesystem::path {
-  if (unidentify) {
-    return "unidentified";
-  } else if (bundle) {
-    return "bundles";
-  } else {
-    return "schemas";
-  }
-}
-
-static auto resolver(std::string_view identifier, const bool bundle,
-                     const bool unidentify)
-    -> std::optional<sourcemeta::core::JSON> {
-  static const auto SERVER_BASE_URL{
-      sourcemeta::core::URI{configuration().at("url").to_string()}
-          .canonicalize()};
-  sourcemeta::core::URI uri{std::string{identifier}};
-  uri.canonicalize().relative_to(SERVER_BASE_URL);
-
-  // If so, this URI doesn't belong to us
-  if (uri.is_absolute() || uri.empty()) {
-    return sourcemeta::core::schema_official_resolver(identifier);
-  }
-
-  assert(uri.path().has_value());
-  const auto schema_path{
-      path_join(*(__global_data) / schema_directory(bundle, unidentify),
-                uri.path().value())};
-  if (!std::filesystem::exists(schema_path)) {
-    return std::nullopt;
-  }
-
-  return sourcemeta::core::read_json(schema_path);
-}
 
 static auto json_error(const ServerLogger &logger, uWS::HttpRequest *,
                        ServerResponse &response,
@@ -161,6 +108,44 @@ static auto on_static(const ServerLogger &logger, uWS::HttpRequest *request,
   serve_file(asset_path.str(), request, response);
 }
 
+static auto schema_directory_prefix(uWS::HttpRequest *request) noexcept
+    -> std::filesystem::path {
+  // Because Visual Studio Code famously does not support `$id` or `id`
+  // See https://github.com/microsoft/vscode-json-languageservice/issues/224
+  const auto &user_agent{request->getHeader("user-agent")};
+  const auto is_vscode{user_agent.starts_with("Visual Studio Code") ||
+                       user_agent.starts_with("VSCodium")};
+  const auto bundle{!request->getQuery("bundle").empty()};
+  const auto unidentify{!request->getQuery("unidentify").empty()};
+
+  if (unidentify || is_vscode) {
+    return "unidentified";
+  } else if (bundle) {
+    return "bundles";
+  } else {
+    return "schemas";
+  }
+}
+
+static auto read_schema(uWS::HttpRequest *request) -> std::optional<
+    std::pair<sourcemeta::core::JSON, sourcemeta::core::JSON>> {
+  std::string request_path{request->getUrl()};
+  // Otherwise we may get unexpected results in case-sensitive file-systems
+  std::transform(
+      request_path.begin(), request_path.end(), request_path.begin(),
+      [](const unsigned char character) { return std::tolower(character); });
+  const auto schema_path{*(__global_data) / schema_directory_prefix(request) /
+                         request_path.substr(1)};
+  if (!std::filesystem::exists(schema_path)) {
+    return std::nullopt;
+  }
+
+  const auto metadata_path{schema_path.string() + ".meta"};
+  assert(std::filesystem::exists(metadata_path));
+  return std::make_pair(sourcemeta::core::read_json(metadata_path),
+                        sourcemeta::core::read_json(schema_path));
+}
+
 static auto on_request(const ServerLogger &logger, uWS::HttpRequest *request,
                        ServerResponse &response) -> void {
   std::string request_path{request->getUrl()};
@@ -169,8 +154,8 @@ static auto on_request(const ServerLogger &logger, uWS::HttpRequest *request,
       [](const unsigned char character) { return std::tolower(character); });
 
   if (!request_path.ends_with(".json")) {
-    const auto directory_path{
-        path_join(*(__global_data) / "generated", request_path)};
+    const auto directory_path{*(__global_data) / "generated" /
+                              request_path.substr(1)};
     if (std::filesystem::is_directory(directory_path)) {
       serve_file(directory_path / "index.html", request, response);
     } else {
@@ -181,37 +166,15 @@ static auto on_request(const ServerLogger &logger, uWS::HttpRequest *request,
     return;
   }
 
-  const auto bundle{!request->getQuery("bundle").empty()};
-  const auto unidentify{!request->getQuery("unidentify").empty()};
-
-  const auto metadata_path{
-      path_join(*(__global_data) / schema_directory(bundle, unidentify),
-                request_path + ".meta")};
-  if (!std::filesystem::exists(metadata_path)) {
+  const auto schema{read_schema(request)};
+  if (!schema.has_value()) {
     json_error(logger, request, response,
                sourcemeta::hydra::http::Status::NOT_FOUND, "not-found",
                "There is no schema at this URL");
     return;
   }
 
-  const auto metadata{sourcemeta::core::read_json(metadata_path)};
-
-  // Because Visual Studio Code famously does not support `$id` or `id`
-  // See https://github.com/microsoft/vscode-json-languageservice/issues/224
-  const auto user_agent{request->getHeader("user-agent")};
-  const auto is_vscode{user_agent.starts_with("Visual Studio Code") ||
-                       user_agent.starts_with("VSCodium")};
-
-  const auto maybe_schema{resolver(metadata.at("id").to_string(),
-                                   is_vscode || bundle,
-                                   is_vscode || unidentify)};
-  if (!maybe_schema.has_value()) {
-    json_error(logger, request, response,
-               sourcemeta::hydra::http::Status::NOT_FOUND, "not-found",
-               "There is no schema at this URL");
-    return;
-  }
-
+  const auto &metadata{schema.value().first};
   const auto hash{metadata.at("md5").to_string()};
   if (!header_if_none_match(request, hash)) {
     response.status = sourcemeta::hydra::http::Status::NOT_MODIFIED;
@@ -231,8 +194,7 @@ static auto on_request(const ServerLogger &logger, uWS::HttpRequest *request,
   response.header("Last-Modified", metadata.at("lastModified").to_string());
 
   std::ostringstream payload;
-  sourcemeta::core::prettify(maybe_schema.value(), payload,
-                             sourcemeta::core::schema_format_compare);
+  sourcemeta::core::prettify(schema.value().second, payload);
   if (request->getMethod() == "head") {
     response.head(payload.str());
     return;
@@ -253,10 +215,8 @@ static auto on_otherwise(const ServerLogger &logger, uWS::HttpRequest *request,
   }
 
   if (request->getUrl().ends_with(".json")) {
-    const auto metadata_path{
-        path_join(*(__global_data) / schema_directory(false, false),
-                  std::string{request->getUrl()} + ".meta")};
-    if (std::filesystem::exists(metadata_path)) {
+    const auto schema{read_schema(request)};
+    if (schema.has_value()) {
       json_error(logger, request, response,
                  sourcemeta::hydra::http::Status::METHOD_NOT_ALLOWED,
                  "method-not-allowed",
@@ -275,12 +235,6 @@ static auto on_error(std::exception_ptr exception_ptr,
                      ServerResponse &response) noexcept -> void {
   try {
     std::rethrow_exception(exception_ptr);
-  } catch (const sourcemeta::core::SchemaResolutionError &error) {
-    std::ostringstream message;
-    message << error.what() << ": " << error.id();
-    json_error(logger, request, response,
-               sourcemeta::hydra::http::Status::BAD_REQUEST,
-               "schema-resolution-error", message.str());
   } catch (const std::exception &error) {
     json_error(logger, request, response,
                sourcemeta::hydra::http::Status::METHOD_NOT_ALLOWED,
@@ -328,11 +282,13 @@ auto main(int argc, char *argv[]) noexcept -> int {
     server.otherwise(on_otherwise);
     server.error(on_error);
 
-    assert(configuration().defines("port"));
-    assert(configuration().at("port").is_integer());
-    assert(configuration().at("port").is_positive());
+    const auto configuration{
+        sourcemeta::core::read_json(*__global_data / "configuration.json")};
+    assert(configuration.defines("port"));
+    assert(configuration.at("port").is_integer());
+    assert(configuration.at("port").is_positive());
     return server.run(
-        static_cast<std::uint32_t>(configuration().at("port").to_integer()));
+        static_cast<std::uint32_t>(configuration.at("port").to_integer()));
   } catch (const std::exception &error) {
     std::cerr << "unexpected error: " << error.what() << "\n";
     return EXIT_FAILURE;
