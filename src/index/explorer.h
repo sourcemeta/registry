bcba0002178b72c8a1448852c53f727740434ce1
Juan Cruz Viotti <jv@jviotti.com>
2025-08-13 13:25:17 -0400

    More gracefully handler `.schema` extensions in navigation tables
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/index/explorer.h
+++ src/index/explorer.h
@@ -688,7 +688,12 @@ auto GENERATE_NAV_DIRECTORY(const sourcemeta::core::JSON &configuration,
     } else if (entry.is_directory() &&
                std::filesystem::exists(entry.path() / "%" /
                                        "schema.metapack")) {
-      entry_json.assign("name", sourcemeta::core::JSON{entry.path().stem()});
+      auto name{entry.path().stem()};
+      if (name.extension() == ".schema") {
+        name.replace_extension("");
+      }
+
+      entry_json.assign("name", sourcemeta::core::JSON{std::move(name)});
       auto schema_nav_path{navigation_base / entry_relative_path};
       schema_nav_path.replace_extension("");
       schema_nav_path /= "%";
