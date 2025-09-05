475a2f9d514a8440a2c71c0465518f2f5eac0eb9
Juan Cruz Viotti <jv@jviotti.com>
2025-09-05 13:37:07 -0400

    Avoid re-compiling `template.json` every time (#428)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/configuration/compile.cmake
+++
@@ -1,8 +0,0 @@
-execute_process(
-  COMMAND "${JSONSCHEMA}" compile --minify "${INPUT}"
-  OUTPUT_FILE "${OUTPUT}"
-  RESULT_VARIABLE RESULT)
-
-if(RESULT)
-  message(FATAL_ERROR "Failed to compile schema: ${INPUT}")
-endif()
