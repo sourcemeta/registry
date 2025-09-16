31169f9a65d9039b19f93a12b09ad62af718b66a
Juan Cruz Viotti <jv@jviotti.com>
2025-09-16 14:33:19 -0400

    Move `sourcemeta/registry/common` schemas in to `/std`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ collections/std/v0.0.1/registry.cmake
@@ -0,0 +1,11 @@
+set(COLLECTION_NAMESPACE "std/v0.0.1")
+
+install(DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/schemas"
+  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/registry/collections/${COLLECTION_NAMESPACE}"
+  COMPONENT sourcemeta_registry)
+install(FILES "${CMAKE_CURRENT_LIST_DIR}/registry.json"
+  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/registry/collections/${COLLECTION_NAMESPACE}"
+  COMPONENT sourcemeta_registry)
+install(FILES "${CMAKE_CURRENT_LIST_DIR}/jsonschema.json"
+  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/registry/collections/${COLLECTION_NAMESPACE}"
+  COMPONENT sourcemeta_registry)
