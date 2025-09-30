3e7c4d943480f1734d6ecc2557cb1efe8bff3b0b
Juan Cruz Viotti <jv@jviotti.com>
2025-09-30 17:19:56 -0400

    Implement cache busting on the `.js` and `.css` files (#521)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/shared/version.cc
+++ src/shared/version.cc
@@ -1,8 +1,15 @@
 #include <sourcemeta/registry/shared_version.h>
 
+#include <sourcemeta/core/uuid.h>
+
 #include "configure.h"
 
 namespace sourcemeta::registry {
+
+auto stamp() noexcept -> std::string_view {
+  static auto CACHE_STAMP{sourcemeta::core::uuidv4()};
+  return CACHE_STAMP;
+}
 
 auto version() noexcept -> std::string_view { return PROJECT_VERSION; }
 
