7ed029b77a089570cb3974e62faace0cd49d5ad7
Juan Cruz Viotti <jv@jviotti.com>
2025-09-30 16:21:47 -0400

    Add provenance information to the schema metadata API (#520)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- collections/openapi/v3.0/registry.cmake
+++ collections/openapi/v3.0/registry.cmake
@@ -13,6 +13,10 @@ install(FILES "${OPENAPI_PATH}/2024-10-18"
 install(FILES "${CMAKE_CURRENT_LIST_DIR}/registry.json"
   DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/registry/collections/${COLLECTION_NAMESPACE}"
   COMPONENT sourcemeta_registry)
-install(FILES "${CMAKE_CURRENT_LIST_DIR}/jsonschema.json"
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/jsonschema.json"
+  "${CMAKE_CURRENT_BINARY_DIR}/${COLLECTION_NAMESPACE}.json" @ONLY)
+install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${COLLECTION_NAMESPACE}.json"
   DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/registry/collections/${COLLECTION_NAMESPACE}"
+  RENAME jsonschema.json
   COMPONENT sourcemeta_registry)
