94a634c11bafad60d6156d4993280e46d6a31c06
Juan Cruz Viotti <jv@jviotti.com>
2025-10-28 12:50:53 -0400

    Deploy Std using only the major version
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ collections/sourcemeta/std/v0/registry.cmake
@@ -0,0 +1,16 @@
+set(COLLECTION_NAMESPACE "sourcemeta/std/v0")
+set(STD_PATH "${PROJECT_SOURCE_DIR}/vendor/collections/${COLLECTION_NAMESPACE}")
+
+install(DIRECTORY "${STD_PATH}/schemas"
+  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/registry/collections/${COLLECTION_NAMESPACE}"
+  COMPONENT sourcemeta_registry)
+install(FILES "${CMAKE_CURRENT_LIST_DIR}/registry.json"
+  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/registry/collections/${COLLECTION_NAMESPACE}"
+  COMPONENT sourcemeta_registry)
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/jsonschema.json"
+  "${CMAKE_CURRENT_BINARY_DIR}/${COLLECTION_NAMESPACE}.json" @ONLY)
+install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${COLLECTION_NAMESPACE}.json"
+  DESTINATION "${CMAKE_INSTALL_DATADIR}/sourcemeta/registry/collections/${COLLECTION_NAMESPACE}"
+  RENAME jsonschema.json
+  COMPONENT sourcemeta_registry)
